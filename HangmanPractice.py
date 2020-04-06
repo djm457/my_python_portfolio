23d12a5760ccc5f8b727cd7c84dbb120ca1dfa0f
David Miller <djm457@gmail.com>
2020-04-05 20:56:44 -0400

    Updated the game engine to print 'you win!' when all the letters are guessed correct. This was accomplished by adding a variable called 'failed' which is set to 0 at the start of the lopp. If all the correct letters are not available at the end of the loop 'failed' is set to 1 allowing the game to continue.

--- HangmanPractice.py
+++ HangmanPractice.py
@@ -1,21 +1,5 @@
 #!/usr/bin/env python3
 
-
-word = "low"
-letters = ["l","o", "w"]
-wordlen = 0
-wordlenB = 0
-print(len(word))
-      
-if all(letter in word for letter in letters):
-    wordlenB +=1
-
-if wordlenB == len(word):
-    print("winner")
-   
-
-"""
-
 print("-"*60)
 print(' Welcome to he hangman game. I am using this to improve my Python capabilities.\n', \
       'The code as the foundation for this script may be found at: \n https://www.pythonforbeginners.com/code-snippets-source-code/game-hangman/ \n',
@@ -34,9 +18,10 @@ solution_display = []
 game_rounds = 7
 
 
+
 #Game engine
 while game_rounds > 0:
-    
+    failed = 0
     guess = input("Guess a character: ")
     if guess in secret_word:
         print('correct guess\n')
@@ -47,15 +32,20 @@ while game_rounds > 0:
                 print(char)
             else:
                 print('-')
+                failed += 1
+               
     else:
         game_rounds -= 1
         guesses.append(guess)
-        print("INCORRECT GUESSES: {} \nCORRECT GUESSES: {} \nREMAINING TURNS: {}".format(guesses, solution_display, game_rounds))
+        print("ALL LETTERS GUESSED: {} \nCORRECT GUESSES: {} \nREMAINING TURNS: {}".format(guesses, solution_display, game_rounds))
 
-if game_rounds == 0: print("You Loser!")
+    if failed == 0:
+        print("You won!")
+        break   
+    if game_rounds == 0:
+        print("You Loser!")
 
-    
-"""
+
    
 
             
